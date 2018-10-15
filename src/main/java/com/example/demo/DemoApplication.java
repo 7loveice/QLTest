49dc85356e3afd2fc077fdb6bf1153233967ac3f
b5mali4 <b5mali4@b5mali4deMacBook-Pro.local>
2018-10-15 10:19:43 +0800

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,14 @@ import demo.ReadObjectDemo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		SpringApplication.run(DemoApplication.class, args);
 		ReadObjectDemo1 readObjectDemo1 = new ReadObjectDemo1();
-		readObjectDemo1.readObject();
+		readObjectDemo1.readObject(null);
 	}
 }
