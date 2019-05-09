98ca8bd18035deeede9b6847dce8d6de3e2892ec
b5mali4 <b5mali4@b5mali4deMacBook-Pro.local>
2019-05-09 17:45:13 +0800

    2333

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+//ba00e74b6e2acf660c0d4b215abbcbb7faf482291
+
 
 @SpringBootApplication
 public class DemoApplication {
